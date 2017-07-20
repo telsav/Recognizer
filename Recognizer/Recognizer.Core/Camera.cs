d3d6de28412645c158b7271f5ae9e84332fc384e
chenglinwang <chenglinwang@CHENGLINWANG>
2017-07-20 19:17:17 +0800

    初始代码

---
+++ Recognizer/Recognizer.Core/Camera.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Recognizer.Core
+{
+    class Camera
+    {
+    }
+}
---
+++ Recognizer/Recognizer.Core/Input.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Recognizer.Core
+{
+    public class Input
+    {
+    }
+}
