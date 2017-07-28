e363bd1fc0f149d127272f70eeed4039ad40e2d9
Charlie Wang <cheng-linw@hotmail.com>
2017-07-28 19:18:31 +0800

    update get stream from rtmp server

--- Recognizer/Recognizer.Lib/camera.py
+++ Recognizer/Recognizer.Lib/camera.py
@@ -4,15 +4,29 @@ import cv2
 from train import Model
 from image_show import show_image
 
+
+#Id=raw_input('enter your id: ')
+#sampleNum=0
+
 # https://realpython.com/blog/python/face-detection-in-python-using-a-webcam/
 if __name__ == '__main__':
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture()
+
+    cv2.namedWindow("preview")
+
+    cap.open('rtmp://123.207.24.118/live/mystream')
+
+    #cap.open('http//123.207.24.118/live/mystream?.mjpeg')
+
     cascade_path = "C:\Program Files\Anaconda3\Library\etc\haarcascades\haarcascade_frontalface_default.xml"
     model = Model()
 
     model.load()
     while True:
         _, frame = cap.read()
+
+        #Test
+        # Display the resulting frame
 
         # 灰度变换
         frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -22,6 +36,19 @@ if __name__ == '__main__':
 
         # 物体识别的执行（面部识别）
         facerect = cascade.detectMultiScale(frame_gray, scaleFactor=1.2, minNeighbors=3, minSize=(10, 10))
+
+        # Draw a rectangle around the faces
+        for (x, y, w, h) in facerect:
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            cv2.putText(frame, "Press ESC to close.", (5, 25),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
+            #incrementing sample number 
+            #sampleNum=sampleNum+1
+            #saving the captured face in the dataset folder
+            #cv2.imwrite("dataSet/user."+Id+'.'+str(sampleNum)+".jpg",gray[y:y+h,x:x+w])
+
+            cv2.imshow("preview", frame)
+
         #facerect = cascade.detectMultiScale(frame_gray, scaleFactor=1.01, minNeighbors=3, minSize=(3, 3))
         if len(facerect) > 0:
             print('face detected')
@@ -37,7 +64,7 @@ if __name__ == '__main__':
                 result = model.predict(image)
                 if result == 0:  # boss
                     print('Boss is approaching')
-                    show_image()
+                    #show_image()
                 else:
                     print('Not boss')
 
